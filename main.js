ee0b1a086c24dcabe171cb2094ee311170603d1a
Matin Nemati <95319513+MatNemati@users.noreply.github.com>
2025-07-15 02:11:14 +0330

    Add main js

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const root = document.getElementById("root");
+
+  root.innerHTML = `
+    <div class="container">
+      <img src="Matin_Nemati.jpg" alt="Matin Nemati" class="profile" />
+      <h1 style="color:#00ffe1;">Hi, I'm Matin Nemati</h1>
+      <p style="color:#ccc; max-width: 400px;">Electrical Engineering student passionate about Neuroscience and futuristic technologies.</p>
+    </div>
+  `;
+});
